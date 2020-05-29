d450a942b64ee919cbbb219bbca4974a7677e009
inksharedsolutions <webdev.inkshared@gmail.com>
2020-05-30 01:41:45 +0800

    changes-5-29-20

--- src/components/mid-main.js
+++ src/components/mid-main.js
@@ -11,7 +11,7 @@ const MidMain = ()=>{
                 <div className="container-gt separator-line">
                     <h1>Featured</h1>
                     <p className="middle-params">
-                        Gardiner Noble has been featured numerous time on
+                        Gardiner Noble has been featured numerous times on..
                     </p>
 
                     <div className="grid-template-four-col" id="logos-col">
